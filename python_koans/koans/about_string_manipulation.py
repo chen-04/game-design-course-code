9218602d18466ef307aeb8043fd55a95dad00cdc
Nancy Chen <ychen_nancy@163.com>
2021-07-23 08:15:12 +0800

    update

--- python_koans/koans/about_string_manipulation.py
+++ python_koans/koans/about_string_manipulation.py
@@ -9,21 +9,13 @@ class AboutStringManipulation(Koan):
         value1 = 'one'
         value2 = 2
         string = "The values are {0} and {1}".format(value1, value2)
-<<<<<<< HEAD
         self.assertEqual("The values are one and 2", string)
-=======
-        self.assertEqual(__, string)
->>>>>>> fbdff9808b65e839ca1ae3ac1fddfbf1a368b727
 
     def test_formatted_values_can_be_shown_in_any_order_or_be_repeated(self):
         value1 = 'doh'
         value2 = 'DOH'
         string = "The values are {1}, {0}, {0} and {1}!".format(value1, value2)
-<<<<<<< HEAD
         self.assertEqual("The values are DOH, doh, doh and DOH!", string)
-=======
-        self.assertEqual(__, string)
->>>>>>> fbdff9808b65e839ca1ae3ac1fddfbf1a368b727
 
     def test_any_python_expression_may_be_interpolated(self):
         import math # import a standard python module with math functions
@@ -31,7 +23,6 @@ class AboutStringManipulation(Koan):
         decimal_places = 4
         string = "The square root of 5 is {0:.{1}f}".format(math.sqrt(5),
             decimal_places)
-<<<<<<< HEAD
         self.assertEqual("The square root of 5 is 2.2361", string)
 
     def test_you_can_get_a_substring_from_a_string(self):
@@ -45,30 +36,11 @@ class AboutStringManipulation(Koan):
     def test_single_characters_can_be_represented_by_integers(self):
         self.assertEqual(97, ord('a'))
         self.assertEqual(True, ord('b') == (ord('a') + 1))
-=======
-        self.assertEqual(__, string)
-
-    def test_you_can_get_a_substring_from_a_string(self):
-        string = "Bacon, lettuce and tomato"
-        self.assertEqual(__, string[7:10])
-
-    def test_you_can_get_a_single_character_from_a_string(self):
-        string = "Bacon, lettuce and tomato"
-        self.assertEqual(__, string[1])
-
-    def test_single_characters_can_be_represented_by_integers(self):
-        self.assertEqual(__, ord('a'))
-        self.assertEqual(__, ord('b') == (ord('a') + 1))
->>>>>>> fbdff9808b65e839ca1ae3ac1fddfbf1a368b727
 
     def test_strings_can_be_split(self):
         string = "Sausage Egg Cheese"
         words = string.split()
-<<<<<<< HEAD
         self.assertListEqual(["Sausage", "Egg", "Cheese"], words)
-=======
-        self.assertListEqual([__, __, __], words)
->>>>>>> fbdff9808b65e839ca1ae3ac1fddfbf1a368b727
 
     def test_strings_can_be_split_with_different_patterns(self):
         import re #import python regular expression library
@@ -78,30 +50,20 @@ class AboutStringManipulation(Koan):
 
         words = pattern.split(string)
 
-<<<<<<< HEAD
         self.assertListEqual(["the", "rain", "in", "spain"], words)
-=======
-        self.assertListEqual([__, __, __, __], words)
->>>>>>> fbdff9808b65e839ca1ae3ac1fddfbf1a368b727
 
         # Pattern is a Python regular expression pattern which matches ',' or ';'
 
     def test_raw_strings_do_not_interpret_escape_characters(self):
         string = r'\n'
         self.assertNotEqual('\n', string)
-<<<<<<< HEAD
         self.assertEqual(r'\n', string)
         self.assertEqual(2, len(string))
-=======
-        self.assertEqual(__, string)
-        self.assertEqual(__, len(string))
->>>>>>> fbdff9808b65e839ca1ae3ac1fddfbf1a368b727
 
         # Useful in regular expressions, file paths, URLs, etc.
 
     def test_strings_can_be_joined(self):
         words = ["Now", "is", "the", "time"]
-<<<<<<< HEAD
         self.assertEqual("Now is the time", ' '.join(words))
 
     def test_strings_can_change_case(self):
@@ -110,13 +72,3 @@ class AboutStringManipulation(Koan):
         self.assertEqual('timbot', 'TimBot'.lower())
         self.assertEqual('Guido Van Rossum', 'guido van rossum'.title())
         self.assertEqual('tOtAlLy AwEsOmE', 'ToTaLlY aWeSoMe'.swapcase())
-=======
-        self.assertEqual(__, ' '.join(words))
-
-    def test_strings_can_change_case(self):
-        self.assertEqual(__, 'guido'.capitalize())
-        self.assertEqual(__, 'guido'.upper())
-        self.assertEqual(__, 'TimBot'.lower())
-        self.assertEqual(__, 'guido van rossum'.title())
-        self.assertEqual(__, 'ToTaLlY aWeSoMe'.swapcase())
->>>>>>> fbdff9808b65e839ca1ae3ac1fddfbf1a368b727
